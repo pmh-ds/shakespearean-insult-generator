2752edd17ae86975cbce628e9486125e882ba10b
Paul Harrison <paulharrison2@gmail.com>
2017-07-18 15:40:27 +0000

    Read insults.

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
+import csv
 from flask import Flask
 app = Flask(__name__)
 
+insults_start = []
+insults_middle = []
+insults_end = []
+
+with open("insults.csv") as csvfile:
+    insult_reader = csv.reader(csvfile, delimiter=",")
+    for row in insult_reader:
+        start, middle, end = row
+        insults_start.append(start)
+        insults_middle.append(middle)
+        insults_end.append(end)
+    
+print(insults_start)
 
 @app.route('/')
 def hello():
