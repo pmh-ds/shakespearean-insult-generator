8aea34aca588313390be4fcc757cd2f6405a68f4
Paul Harrison <paulharrison2@gmail.com>
2017-07-29 22:55:58 +0100

    TiRefactor read_insults.

--- shakespearean_insult_generator/views.py
+++ shakespearean_insult_generator/views.py
@@ -1,24 +1,21 @@
-import csv
 from random import randint
 from shakespearean_insult_generator import app
 
 
 def read_insults(insults_path):
-    insults_start = []
-    insults_middle = []
-    insults_end = []
+    insults_start, insults_middle, insults_end = [], [], []
     with open(insults_path) as csvfile:
-        insult_reader = csv.reader(csvfile, delimiter=",")
-        for row in insult_reader:
-            start, middle, end = row
+        insults = csvfile.readlines()
+        for row in insults:
+            start, middle, end = row.strip().split(',')
             insults_start.append(start)
             insults_middle.append(middle)
             insults_end.append(end)
 
-    return insults_start, insults_middle,  insults_end
+    return insults_start, insults_middle, insults_end
 
 
-insults_start, insults_middle,  insults_end = read_insults("insults.csv")
+insults_start, insults_middle, insults_end = read_insults("insults.csv")
 
 
 def generate_insult(insults_start, insults_middle,  insults_end):
