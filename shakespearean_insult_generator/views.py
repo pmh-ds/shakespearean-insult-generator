5304452435e5068beb5cb33dbc9d5d885d0425d4
Paul Harrison <paulharrison2@gmail.com>
2017-07-29 22:10:18 +0100

    Tidy code.

--- shakespearean_insult_generator/views.py
+++ shakespearean_insult_generator/views.py
@@ -2,6 +2,7 @@ import csv
 from random import randint
 from shakespearean_insult_generator import app
 
+
 def read_insults(insults_path):
     insults_start = []
     insults_middle = []
@@ -13,20 +14,29 @@ def read_insults(insults_path):
             insults_start.append(start)
             insults_middle.append(middle)
             insults_end.append(end)
-    
+
     return insults_start, insults_middle,  insults_end
 
+
 insults_start, insults_middle,  insults_end = read_insults("insults.csv")
+
 
 def generate_insult(insults_start, insults_middle,  insults_end):
     return insults_start[randint(0, len(insults_start)-1)] + " "\
         + insults_middle[randint(0, len(insults_middle)-1)] + " "\
         + insults_end[randint(0, len(insults_end)-1)] + " "
-    
+
+
 @app.route('/')
 def insult():
-    return "Thou {insult}".format(insult=generate_insult(insults_start, insults_middle,  insults_end))
-    
+    return "Thou {insult}".format(insult=generate_insult(insults_start,
+                                                         insults_middle,
+                                                         insults_end))
+
+
 @app.route('/<name>')
 def insult_name(name):
-    return "{name}, thou {insult}".format(name=name, insult=generate_insult(insults_start, insults_middle,  insults_end))
+    return "{name}, thou {insult}".format(name=name,
+                                          insult=generate_insult(insults_start,
+                                                                 insults_middle,
+                                                                 insults_end))
--- tests.py
+++ tests.py
@@ -1,22 +1,24 @@
 from shakespearean_insult_generator import app
 import unittest
 
+
 class ShakespeareanInsultGeneratorTests(unittest.TestCase):
-    
+
     def setUp(self):
         self.app = app.test_client()
         self.app.testing = True
-    
+
     def tearDown(self):
         pass
-    
+
     def test_insult_status_code(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-        
+
     def test_insult_name_status_code(self):
         response = self.app.get('/Frodo')
         self.assertEqual(response.status_code, 200)
-        
+
+
 if __name__ == "__main__":
     unittest.main()
