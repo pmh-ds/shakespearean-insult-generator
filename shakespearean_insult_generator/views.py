dd066962d4288bad1f9722ab7a73806f3f06035c
Paul Harrison <paulharrison2@gmail.com>
2017-07-29 06:21:20 +0000

    Read insults in function.

--- shakespearean_insult_generator/views.py
+++ shakespearean_insult_generator/views.py
@@ -2,27 +2,31 @@ import csv
 from random import randint
 from shakespearean_insult_generator import app
 
-# read insults
-insults_start = []
-insults_middle = []
-insults_end = []
-with open("insults.csv") as csvfile:
-    insult_reader = csv.reader(csvfile, delimiter=",")
-    for row in insult_reader:
-        start, middle, end = row
-        insults_start.append(start)
-        insults_middle.append(middle)
-        insults_end.append(end)
+def read_insults(insults_path):
+    insults_start = []
+    insults_middle = []
+    insults_end = []
+    with open(insults_path) as csvfile:
+        insult_reader = csv.reader(csvfile, delimiter=",")
+        for row in insult_reader:
+            start, middle, end = row
+            insults_start.append(start)
+            insults_middle.append(middle)
+            insults_end.append(end)
+    
+    return insults_start, insults_middle,  insults_end
 
-def generate_insult():
+insults_start, insults_middle,  insults_end = read_insults("insults.csv")
+
+def generate_insult(insults_start, insults_middle,  insults_end):
     return insults_start[randint(0, len(insults_start)-1)] + " "\
         + insults_middle[randint(0, len(insults_middle)-1)] + " "\
         + insults_end[randint(0, len(insults_end)-1)] + " "
     
 @app.route('/')
 def insult():
-    return "Thou {insult}".format(insult=generate_insult())
+    return "Thou {insult}".format(insult=generate_insult(insults_start, insults_middle,  insults_end))
     
 @app.route('/<name>')
 def insult_name(name):
-    return "{name}, thou {insult}".format(name=name, insult=generate_insult())
+    return "{name}, thou {insult}".format(name=name, insult=generate_insult(insults_start, insults_middle,  insults_end))
