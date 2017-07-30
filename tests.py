f0aaa3f8279ee829599a931cc90e0d830be95736
Paul Harrison <paulharrison2@gmail.com>
2017-07-30 17:19:19 +0000

    Rework tests to use Flask-Testing.

--- tests.py
+++ tests.py
@@ -1,15 +1,20 @@
 import unittest
+import urllib2
+from flask import Flask
+from flask_testing import TestCase
 from mock import mock_open, patch
 
 from shakespearean_insult_generator import app
-from shakespearean_insult_generator.views import read_insults, generate_insult, insult, insult_name
+from shakespearean_insult_generator.views import read_insults, generate_insult
 
 
-class ShakespeareanInsultGeneratorTests(unittest.TestCase):
+class ShakespeareanInsultGeneratorTests(TestCase):
+    
+    def create_app(self):
+        app.config['TESTING'] = True
+        return app
 
     def setUp(self):
-        self.app = app.test_client()
-        self.app.testing = True
         self.test_name = 'Frodo'
         self.test_insults_start = ["artless"]
         self.test_insults_middle = ["base-court"]
@@ -24,25 +29,29 @@ class ShakespeareanInsultGeneratorTests(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_insult_status_code(self):
-        response = self.app.get('/')
+    def test_insult_route(self):
+        response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
-    def test_insult_name_status_code(self):
-        response = self.app.get('/{test_name}'.format(test_name=self.test_name))
+    def test_insult_name_route(self):
+        response = self.client.get('/{test_name}'.format(test_name=self.test_name))
         self.assertEqual(response.status_code, 200)
 
     def test_insult(self):
         with patch('shakespearean_insult_generator.views.generate_insult',
                    return_value=self.test_insult):
-            self.assertEqual(insult(), "Thou {insult}".format(insult=self.test_insult))
+            self.client.get('/')
+            self.assert_template_used('index.html')
+            self.assert_context('insult',  "Thou {insult}".format(insult=self.test_insult))
 
     def test_insult_name(self):
         with patch('shakespearean_insult_generator.views.generate_insult',
                    return_value=self.test_insult):
-            self.assertEqual(insult_name(self.test_name),
-                             "{test_name}, thou {insult}".format(test_name=self.test_name,
-                                                                 insult=self.test_insult))
+            self.client.get('/{name}'.format(name=self.test_name))
+            self.assert_template_used('index.html')
+            self.assert_context('insult',
+                                "{test_name}, thou {insult}".format(test_name=self.test_name,
+                                                                    insult=self.test_insult))
 
     def test_read_insult(self):
         mocked_open = mock_open(read_data=self.test_insult_file)
@@ -62,5 +71,5 @@ class ShakespeareanInsultGeneratorTests(unittest.TestCase):
                          self.test_insult)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
